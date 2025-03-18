02596d3594d799da60f45a83a64a76f808fb05f7
angryCar <ouabbasaymen@gmail.com>
2025-03-18 07:38:18 +0100

    adding the comment to the javascript code

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
-const randomNumber = Math.floor(Math.random() * 100) + 1
+//declaring The Answer and it's a random number (int) 
+const answer = Math.floor(Math.random() * 100) + 1
 
+//declaring the attempt variable and give it a value and in this case it's zero
 let attempts = 0;
-let guess;
 
+//declaring the items by the id
 const guessInput = document.getElementById('guessInput');
-let submitGuess = document.getElementById('submitGuess');
+const submitGuess = document.getElementById('submitGuess');
 const feedback = document.getElementById('feedback');
 const attemptsDisplay = document.getElementById('attempts');
 
+//checking if the submit button is pressed
 submitGuess.addEventListener('click', () => {
     const userGuess = parseInt(guessInput.value);
-
+    //checking if the user guess is not a number or greater then 100 or lower then 1 
     if(isNaN(userGuess) || userGuess < 1 || userGuess > 100){
-        feedback.textContent = "Please Enter a valid Number";
-        feedback.style.color = 'red';
-        return;
+        feedback.textContent = "Please Enter a valid Number"; //Sets feedback text to prompt the user for valid input
+        feedback.style.color = 'red'; //seting the text color to red
+        return; //exits the function 
     }
-    else if (userGuess < randomNumber){
-        feedback.textContent = `Too low! Try again.`;
-        feedback.style.color = 'orange';
+    //checking if the user guess is lower then the answer
+    else if (userGuess < answer){
+        feedback.textContent = `Too low! Try again.`; //Sets feedback text to prompt the user for the low number
+        feedback.style.color = 'orange'; //seting the text color to orange
     } 
-    else if (userGuess > randomNumber){
-        feedback.textContent = `Too high! Try again.`;
-        feedback.style.color = 'orange';
+    else if (userGuess > answer){
+        feedback.textContent = `Too high! Try again.`; //Sets feedback text to prompt the user for the hight number
+        feedback.style.color = 'orange'; //seting the text color to orange
     }
+    // the player wins 
     else{
-        feedback.textContent = `Congratulations! You guessed the correct number in ${attempts} attempts.`
-        feedback.style.color = 'green';
+        feedback.textContent = `Congratulations! You guessed the correct number in ${attempts} attempts.` //Sets feedback text to prompt that he wins and shows the attempts
+        feedback.style.color = 'green'; //seting the text color to green
     }
 
-    guessInput.value = '';
-
-    attempts++;
+    
+    guessInput.value = ''; //making sure that the input is a zero
+    attempts++; //increasing the attempts by one
 }) 
